0b42f5f50c97706d3931daae00809785624c80b7
Mitchel Pawirodinomo <pawiromitchel@gmail.com>
2021-05-27 08:02:59 -0300

    testing health factor

--- app.js
+++ app.js
@@ -9,15 +9,29 @@ bot.on("polling_error", (msg) => console.log(msg));
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     let text = msg.text ? msg.text : '';
-    let network = text.split(' ');
+    let args = text.split(' ');
 
     if (text.includes('/gas')) {
-        bot.sendMessage(chatId, await FUNCTIONS.getGasPrices(network[1] ? network[1] : ''))
+        let network = args[1];
+        bot.sendMessage(chatId, await FUNCTIONS.getGasPrices(network ? network : ''))
         .then((result) => {
             setTimeout(() => {
                 bot.deleteMessage(chatId, result.message_id)
             }, 10 * 1000)
         })
         .catch(err => console.log(err))
+    }
+
+    if (text.includes('/health')) {
+        let network = args[1];
+        let protocol = args[2];
+        let address = args[3];
+
+        // geen van ze mag leeg zijn
+        if(network && protocol && address) {
+            bot.sendMessage(chatId, await FUNCTIONS.getHealthFactor(protocol, network, address))
+        } else {
+            bot.sendMessage(chatId, 'Please provide the right parameters\nFor example /health polygon aave 0x...')
+        }
     }
 });
--- functions.js
+++ functions.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const CONFIG = require('./config');
+const ZAPPER_ENDPOINT = "https://api.zapper.fi/v1/";
 
 async function getGasPrices(network = '') {
     switch (network) {
@@ -21,7 +22,7 @@ async function getGasPrices(network = '') {
             break;
     }
 
-    const res = await axios.get(`https://api.zapper.fi/v1/gas-price?network=${network}&api_key=${CONFIG.ZAPPER_API_KEY}`);
+    const res = await axios.get(`${ZAPPER_ENDPOINT}gas-price?network=${network}&api_key=${CONFIG.ZAPPER_API_KEY}`);
     const data = res.data;
     return `::GAS PRICES | ${network.toUpperCase()}::
 🟢 Instant: ${data.instant}
@@ -30,6 +31,19 @@ async function getGasPrices(network = '') {
 `
 }
 
-async function getHealthFactor(protocol, network, address) {}
+async function getHealthFactor(protocol, network, address) {
+    let text = `::HEALTH FACTOR::\n`;
+    if(protocol === "aave" && network === "polygon") {
+        protocol = "aave-v2";
+        const res = await axios.get(`${ZAPPER_ENDPOINT}protocols/${protocol}/balances?addresses%5B%5D=${address}&network=${network}&api_key=${CONFIG.ZAPPER_API_KEY}`);
+        const data = res.data;
+        const health = (Object.values(data)[0].products[0].meta[0].value).toFixed(2);
+        text += `${health > 1 ? '🟢': '🔴'} ${protocol.toUpperCase()} on ${network.toUpperCase()}: ${health}`
+    } else {
+        return 'Sorry, @pawiromitchel did not teach me that yet';
+    }
+    
+    return text;
+}
 
-module.exports = { getGasPrices }
+module.exports = { getGasPrices, getHealthFactor }
