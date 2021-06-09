203ce0bcab5dde182c364673de8c20d334ea90be
Mitchel Pawirodinomo <pawiromitchel@gmail.com>
2021-06-09 11:48:42 -0300

    hack.chat doesn't support special chars

--- functions.js
+++ functions.js
@@ -57,7 +57,7 @@ async function getProtocolInfo(address, protocol, network) {
 
 function randomString(length) {
     var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
     for (var i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() *
